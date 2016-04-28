008dc0d92e9548bd66d1887bf032af673584c701
Janika <janika.liiv@gmail.com>
2016-04-28 11:14:00 +0300

    changes to invoice template

--- handlers/invoice.js
+++ handlers/invoice.js
@@ -9,7 +9,11 @@ const invoiceReq = (renderer) => {
       "right": "1in",
       "bottom": "0in",
       "left": "1in"
-    }
+    },
+    "footer": {
+      "height": "28mm",
+      "contents": '<span style="color: #444;">Clanbeat<br />support@clanbeat.com</span>'
+    },
   };
   const getFn = (req, res) => {
     if(!req.body) return res.status(500).json({error_message: 'problem parsing json body'});
