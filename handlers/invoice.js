f8b539e0934f0c2e9d9888da8677bf68454d38b8
Janika <janika.liiv@gmail.com>
2016-06-14 11:08:20 +0300

    fix footer rendering

--- handlers/invoice.js
+++ handlers/invoice.js
@@ -12,16 +12,7 @@ const invoiceReq = (renderer) => {
     },
     "footer": {
       "height": "28mm",
-      "contents": '<div style="color: #444;"">
-          <div style="loat:left;text-align:left;">
-          Clanbeat Inc <br />
-          548 Market st. 63570 <br />
-          San Fransisco, Ca 94104 <br />
-          </div>
-          <div style="float:right;text-align:right;">
-          www.clanbeat.com
-          </div>
-        </div>'
+      "contents": "<div style='color:#444;'><div style='Float:left;text-align:left;'>Clanbeat Inc <br />548 Market st. 63570 <br />San Fransisco, Ca 94104 <br /></div><div style='float:right;text-align:right;'>www.clanbeat.com</div></div>"
     },
   };
   const getFn = (req, res) => {
